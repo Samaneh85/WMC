94b8420c93b7895f895c566865a2bf22874d423b
Samaneh85 <mad240748@spengergasse.at>
2025-05-12 12:54:10 +0200

    Add files via upload

---
+++ backend.js
@@ -0,0 +1,9 @@
+function startGame() {
+  let user = "Lena";
+  let level = 1;
+  debugger;
+  level += 1;
+  console.log(`User: ${user}, Level: ${level}`);
+}
+
+startGame();
--- script.js
+++ script.js
@@ -1,24 +1,7 @@
-function isPrime(num) {
-  if (!Number.isInteger(num) || num < 2) {
-    return { text: "Ungültiger Wert", isError: true };
-  }
-  for (let i = 2; i <= Math.sqrt(num); i++) {
-    if (num % i === 0) {
-      return { text: "Keine Primzahl", isError: false };
-    }
-  }
-  return { text: "Primzahl", isError: false };
-}
+let level = 1;
 
-function checkPrime() {
-  let input = document.getElementById("numberInput").value.trim();
-  let number = Number(input); // Sicherstellen, dass es eine Zahl ist
-
-  console.log("Eingegebene Zahl:", number); // Debugging
-
-  let result = isPrime(number);
-  let resultElement = document.getElementById("result");
-
-  resultElement.innerText = result.text;
-  resultElement.className = result.isError ? "error" : "";
+function increase() {
+  debugger;
+  level++;
+  console.log("Level:", level);
 }
